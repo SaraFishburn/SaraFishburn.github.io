a378a71d550ce796b4304be2134f9eba2c0b0851
Sara Fishburn <fishburn.sara@gmail.com>
2021-03-09 12:14:58 +1000

    resume page created

--- src/static/javascript/global.js
+++ src/static/javascript/global.js
@@ -1,10 +1,7 @@
 // apply the following after the page has loaded
 window.addEventListener('DOMContentLoaded', function() {
   document.getElementById('hamburger').onclick = function() {
-
-    // navbar slide up to reveal page
-    document.getElementById('nav-links').classList.remove('nav-links-show')
-    document.getElementById('drip-top').classList.toggle('drip-top-slide')
+  
 
     // toggle hamburger animation
     document.getElementById('ham-top').classList.toggle('ham-top-transition')
